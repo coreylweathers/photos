697e48138de8ba01a786e534642fe66786ca24c7
Corey Weathers <clw895@msn.com>
2020-06-16 18:51:27 +0000

    Add the Twilio Connect controller to repo

---
+++ Photos.API/Controllers/TwilioController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Photos.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TwilioController : ControllerBase
+    {
+        [HttpGet("authorize"), HttpPost("authorize")]
+        public IActionResult Authorize([FromQuery,FromForm]string accountSid)
+        {
+            // TODO: SAVE THE ACCOUNT SID FOR THE LOGGED IN USER. IF USER NOT LOGGED IN,
+            return NoContent();
+        }
+
+        [HttpGet("deauthorize")]
+        public IActionResult Deauthorize()
+        {
+            // TODO: FILL THIS IN
+            return NoContent();
+        }
+    }
+}
---
+++ Photos.Tests/ApiTests/AuthorizeControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Photos.API.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Photos.Tests.ApiTests
+{
+    public class AuthorizeControllerTests
+    {
+        private readonly TwilioController _sut;
+
+        public AuthorizeControllerTests()
+        {
+            _sut = new TwilioController();
+        }
+
+        [Fact]
+        public void Get_WithNoParams_ReturnsValidAuthorizeResult()
+        {
+            // ARRANGE
+            var accountSid = "AC24504e9f04164744a970e26bb81cb2e8";
+
+            // ACT
+            var result = _sut.Authorize(accountSid) as NoContentResult;
+
+            // ASSERT
+            Assert.True(typeof(NoContentResult) == result.GetType());
+        }
+
+        [Fact]
+        public void Get_WithParams_ReturnsValidDeauthroizeResult()
+        {
+            // ARRANGE
+
+            // ACT
+            var result = _sut.Deauthorize() as NoContentResult;
+
+            // ASSERT
+            Assert.True(typeof(NoContentResult) == result.GetType());
+        }
+    }
+}
